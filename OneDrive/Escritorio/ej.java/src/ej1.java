e793b3ddd2838ec415aee7946b617fadc7f8b08b
Ashley Obregón <ashley.obregon897@std.uni.edu.ni>
2025-06-24 18:43:47 -0600

    Primer commit del archivo ej.java

---
+++ OneDrive/Escritorio/ej.java/src/ej1.java
@@ -0,0 +1,10 @@
+public class ej1 {
+    public static void main(String[] args) {
+        String nombre = "Estudiante";
+        int a=10;
+        int b=0;
+        int suma = a + b;
+        System.out.println("Hola, " + nombre + "!");
+        System.out.println("La suma de " + a + "+" + b + " es: " + suma);
+    }
+}
