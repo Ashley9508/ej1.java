965724ce43b8c9cf1aa3e14d1a2f0d8905ed6c11
Ashley Obregón <ashley.obregon897@std.uni.edu.ni>
2025-06-25 12:29:47 -0600

    cambio saludo

--- src/ej1.java
+++ src/ej1.java
@@ -3,9 +3,9 @@ public class ej1 {
         String nombre = "Estudiante";
         int a=10;
         int b=0;
-        int mul = a * b;
+        int suma = a + b;
         System.out.println("Hola, " + nombre + "!");
-        System.out.println("La multiplicación de " + a + "*" + b + " es: " + mul);
+        System.out.println("La suma de " + a + "+" + b + " es: " + suma);
         System.out.println("Ashley Alexandra Obregón Osorio 2025-1020U");
     }
 }
